9cb5e9318da0061d6b1ba3ecf3795a3baff2e963
John Hart <neoheurist@gmail.com>
2021-09-05 18:04:53 -0400

    Random changes of an undefined nature (need to be reviewed)

--- src/test.ts
+++ src/test.ts
@@ -6,27 +6,34 @@ import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
+
+import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
 
-// Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
-declare var __karma__: any;
-declare var require: any;
+// Unfortunately there's no typing for the `__karma__` variable. Just declare it as any
 
-// Prevent Karma from running prematurely.
+declare const __karma__: any;
+declare const require: any;
+
+// Prevent Karma from running prematurely
+
 __karma__.loaded = function () {};
 
-// First, initialize the Angular testing environment.
+// First, initialize the Angular testing environment
+
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
-// Then we find all the tests.
+
+// Then we find all the tests
+
 const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
+
+// And load the modules
+
 context.keys().map(context);
-// Finally, start Karma to run the tests.
+
+// Finally, start Karma to run the tests
+
 __karma__.start();
